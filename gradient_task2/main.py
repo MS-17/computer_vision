5f7ffc2cbb7bc00c8d73e5ba75a4a49edc2e5513
Sergei <106600613+MS-17@users.noreply.github.com>
2023-12-10 12:05:41 +0800

    modify functions that get diagonal gradient

--- gradient_task2/main.py
+++ gradient_task2/main.py
@@ -1,6 +1,40 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy.ndimage import rotate
 
+
+def plot_images(images: dict, nrows: int = 1, ncols: int = 1) -> None:
+	"""
+		Description: 
+			plot multiple images in a single window
+		Parameters:
+			images: {"image_title": {"data": 2Darray}}
+			nrows: a number of rows the window
+			ncols: a number of columns in the window
+	"""
+
+	if nrows <= 0 or ncols <= 0:
+		raise ValueError("nrows or ncols should be greater than 0")
+
+	fig, axes = plt.subplots(nrows=nrows, ncols=ncols)
+
+	if nrows == 1 and ncols == 1:
+		axes = [axes]
+	elif nrows != 1:
+		# axes.ravel() flattens the 2D array of axes 
+		axes = axes.ravel()
+
+	for idx, title in enumerate(images):
+		if idx + 1 > nrows * ncols:
+			break
+		# get the current axis via its index
+		axis = axes[idx]
+		# display a picture in the axis and set picture's title
+		axis.imshow(images[title]["data"])
+		axis.set_title(title)
+		# axis.set_axis_off()
+
+	plt.show()
 
 
 # predicts v[x, y] between v0=[x0, y0] and v1=[x1, y1], t is a ratio
@@ -38,15 +72,20 @@ def get_vertical_grad(img, color1, color2):
 		# fill i row with r+g+b color
 		image[i, :, :] = [r, g, b]
 	return image
-from scipy.ndimage import rotate
 
-def get_diagonal_grad(img, color1, color2):
+
+# the first approach to get the diagonal gradient (with swapaxes)
+def get_diagonal_grad_first(img, color1, color2):
 	image = img.copy()
 	image = np.swapaxes(image, 0, 2)
-	print(image.shape)
+	# print(image.shape)
+	
+	# I need 199 values 
+	# like if we want to fill 3*3 matrix we'll do:
+	# [[1, 2, 3,], [2, 3, 4], [3, 4, 5]] so that the values are distributed on the diagonals
+	# the total number of diagonals in a square matrix is 2 * shape[0] - 1, so we need in case of 3 * 3 5 values
+	ratios = np.linspace(0, 1, 2 * image.shape[1] - 1)	
 
-	ratios = np.linspace(0, 1, image.shape[1])
-	# print(ratios.shape)
 	vals = []
 	for i, t in enumerate(ratios):
 		r = lerp(color1[0], color2[0], t)
@@ -54,158 +93,59 @@ def get_diagonal_grad(img, color1, color2):
 		b = lerp(color1[2], color2[2], t)
 		vals.append([r, g, b])
 
-	print(vals)
+	for i in range(image.shape[1]):
 
-	for i in range(img.shape[1]):
-		# image[:, j, k] = [r, g, b]
-		...
+		# interesting effect 
+		# image[:, i, :] = vals[i:img.shape[1] + i]
 
-
+		image[:, i, :] = np.array(vals[i:img.shape[1] + i]).T
+	
 	return np.swapaxes(image, 0, 2)
+
+
+# the second approach to get the diagonal gradient (without swapaxes)
+def get_diagonal_grad_second(img, color1, color2):
+	image = img.copy()
+
+	ratios = np.linspace(0, 1, 2 * image.shape[1] - 1)	
+
+	vals = []
+	for i, t in enumerate(ratios):
+		r = lerp(color1[0], color2[0], t)
+		g = lerp(color1[1], color2[1], t)
+		b = lerp(color1[2], color2[2], t)
+		vals.append([r, g, b])
+
+	for i in range(image.shape[0]):
+		image[i, :, :] = vals[i : image.shape[0] + i]
+	return image
 
 
 def main():
 
 	size = 100
-	# 100 * 100 * 3 [255, 128, 0] * 100 is just one layer and there're 100 such layers with 
-	# different colors. Each layers is in fact a row in a final image
+	# 100 * 100 * 3. [255, 128, 0] * 100 will be just one layer (in case of vertical gradient)
+	# and there will be 100 such layers with different colors. Each layers is in fact a row 
+	# in a final image
 	image = np.zeros((size, size, 3), dtype="uint8")	
+
 	color1 = [255, 128, 0]
 	color2 = [0, 128, 255]
+	# color1 = [114, 18, 24]
+	# color2 = [2, 195, 250]
+
 	image = get_vertical_grad(image, color1, color2)
-	image = get_diagonal_grad(image, color1, color2)
+	im1 = get_diagonal_grad_first(image, color1, color2)
+	im2 = get_diagonal_grad_second(image, color1, color2)
 	
-	# plt.imshow(image)
-	# plt.show()
+	# print(image)
+	# print(np.swapaxes(im1, 0, 2))
+	# print(np.swapaxes(im2, 0, 2))
 
-
-	# the total amount of diagonals in the square matrix is shape[0] * 2 - 1
-	
-	
-	a = np.array([ [[1, 2, 3,], [4, 5, 6,]],  [[7, 8, 9,], [10, 11, 12]] ])
-	# print(a.shape)
-	# print(a)
-	# print(np.swapaxes(a, 0, 2))
-	# print(a[:, 0, 1])
-	# a = np.flipud(a)
-	# np.diagonal(a, offset=0)[:] = [0, 0, 0]
-	print(a)
-	print(np.diagonal(a, offset=0))
-
-
-
+	imgs = {"Vertical": {"data": image}, "Diagonal1": {"data": im1}, "Diagonal2": {"data": im2}}
+	plot_images(imgs, 1, 3)
 	return 0
 
 
 if __name__ == '__main__':
 	main()
-
-
-
-
-
-
-# may be useful
-def plot_images(images: dict, nrows: int = 1, ncols: int = 1) -> None:
-	"""
-		Description: 
-			plot multiple images in a single window
-		Parameters:
-			images: {"image_title": {"data": 2Darray}}
-			nrows: a number of rows the window
-			ncols: a number of columns in the window
-	"""
-
-	if nrows <= 0 or ncols <= 0:
-		raise ValueError("nrows or ncols should be greater than 0")
-
-	fig, axes = plt.subplots(nrows=nrows, ncols=ncols)
-
-	if nrows == 1 and ncols == 1:
-		axes = [axes]
-	elif nrows != 1:
-		# axes.ravel() flattens the 2D array of axes 
-		axes = axes.ravel()
-
-	for idx, title in enumerate(images):
-		if idx + 1 > nrows * ncols:
-			break
-		# get the current axis via its index
-		axis = axes[idx]
-		# display a picture in the axis and set picture's title
-		axis.imshow(images[title]["data"])
-		axis.set_title(title)
-		# axis.set_axis_off()
-
-	plt.show()
-
-
-def get_nominal_resolution(images: dict) -> dict:
-	"""
-		image: {"image_title": {"max_length": int}, {"data": 2Darray}}
-	"""
-	result = {}
-	for image in images:
-		data = images[image]["data"]
-		# find the longest row that contains ones and count the nominal resolution	
-		sums = data.sum(axis=1)
-		max_pixel_length = max(sums)
-		nominal_resolution = 0
-		if max_pixel_length != 0:
-			nominal_resolution = images[image]["max_length"] / max_pixel_length
-		result[image] = nominal_resolution
-	return result
-
-
-def subtask1(plot_imgs: bool = False) -> None:
-	# imgs is a dict in the following format: {"image_title": {"max_length": int}, {"data": 2Darray}}
-	imgs = {} 
-	for i in range(1, 7):		
-		path = "data/figure" + str(i) + ".txt"
-		imgs[path] = {}
-		imgs[path]["max_length"] = int(np.loadtxt(path, max_rows=1))
-		imgs[path]["data"] = np.loadtxt(path, skiprows=2)
-	
-	if plot_imgs:
-		plot_images(imgs, 2, 3)
-
-	res = get_nominal_resolution(imgs)
-	for title, resolution in res.items():
-		print(title, resolution)
-
-
-def get_transition_vector(images: dict) -> list:
-	result = []
-
-	t  = list(images.keys())
-	im1, im2 = images[t[0]]["data"], images[t[1]]["data"]
-	
-	# get coordinates of all pixels = 1
-	a = np.where(im1 == 1)
-	b = np.where(im2 == 1)
-
-	# create matrices of these coordinates
-	coords1 = np.column_stack((a[0], a[1]))
-	coords2 = np.column_stack((b[0], b[1]))
-	
-	#subtract the matrices and find the transition vector
-	result = np.unique(coords2 - coords1)
-	return result
-
-
-def subtask2(plot_imgs: bool = False) -> None:
-	# imgs is a dict in the following format: {"image_title": {"max_length": int}, {"data": 2Darray}}
-	imgs = {}
-	for i in range(1, 3):		
-		path = "data/img" + str(i) + ".txt"
-		imgs[path] = {}
-		imgs[path]["data"] = np.loadtxt(path, skiprows=2)
-	
-	if plot_imgs:
-		plot_images(imgs, 1, 2)
-
-	res = get_transition_vector(imgs)
-	print(f"Transition vector: ({res[0]}, {res[1]})")
-
-
-
